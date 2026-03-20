1c3b640c384ec20c4e62052e737301bbc79a7831
KienDH <hakienpdu95@gmail.com>
2026-03-20 16:44:55 +0700

    cap nhat

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,96 @@
 @extends('layouts.app')
 
 @section('content')
+    <section class="wrapper-topstory">
+        <div class="news-box tin_host_home" style="padding-bottom: 0">
+            <div class="tieudiem main">
+                <div class="box-news-larger d-flex item-news full-thumb article-topstory">
+                    <div class="thumb-art">
+                        <div class="image image-wrapper">
+                            <a href="https://vntravel.org.vn/le-hoi-banh-mi-viet-nam-2026-mo-rong-khong-gian-giao-thoa-am-thuc-quoc-te-tai-tphcm-a8346.html" class="image image-medium" title="Lễ hội Bánh mì Việt Nam 2026: Mở rộng không gian giao thoa ẩm thực quốc tế tại TP.HCM">
+                                <img class="lazy loaded" alt="Lễ hội Bánh mì Việt Nam 2026: Mở rộng không gian giao thoa ẩm thực quốc tế tại TP.HCM" data-ll-status="loaded" src="https://vntravel.org.vn/zoom/960x576/uploads/images/blog/haingan116/2026/03/19/img-2831-1773903549.JPG">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="content">
+                        <p class="meta-news">
+                            <a class="author-meta" href="https://vntravel.org.vn/user/haingan116">
+                                <img class="user-avatar" src="https://vntravel.org.vn/zoom/156x0/uploads/images/haingan116/2025/07/25/485168043-4060054997601516-2652204820897826730-n-1753417635.jpg" alt="user-avatar">Trần Như Hải Ngân </a>
+                            <span class="time-public">1 ngày trước</span>
+                        </p>
+                        <h3 class="title title-news">
+                            <a href="https://vntravel.org.vn/le-hoi-banh-mi-viet-nam-2026-mo-rong-khong-gian-giao-thoa-am-thuc-quoc-te-tai-tphcm-a8346.html" title="Lễ hội Bánh mì Việt Nam 2026: Mở rộng không gian giao thoa ẩm thực quốc tế tại TP.HCM"> Lễ hội Bánh mì Việt Nam 2026: Mở rộng không gian giao thoa ẩm thực quốc tế tại TP.HCM </a>
+                        </h3>
+                        <p class="snippet fs3 f-rr"> Sáng 19/3, tại TP.HCM, Hiệp hội Du lịch TP.HCM công bố Lễ hội Bánh mì Việt Nam lần thứ 4 năm 2026 sự kiện tiếp tục khẳng định vị thế của bánh mì không chỉ là món ăn đường phố quen thuộc mà còn là biểu tượng văn hóa có khả năng kết nối toàn cầu. </p>
+                    </div>
+                </div>
+                <div class="sub-news-top">
+                    <ul class="row list-sub-feature fs3">
+                        <div class="col-12 col-md-4">
+                            <!--article_style_image.blade.php-->
+                            <div class="box-news grid-item match-height  kind-van-ban" style="height: 259.375px;">
+                                <div class="image image-wrapper">
+                                    <a href="https://vntravel.org.vn/94-doanh-nhan-tai-viet-nam-du-bao-kinh-te-se-khoi-sac-trong-nam-nay-a8342.html" class="image image-small" title="94% doanh nhân tại Việt Nam dự báo kinh tế sẽ khởi sắc trong năm nay">
+                                        <img src="https://vntravel.org.vn/zoom/480x288/uploads/images/2026/03/19/doanh-nhan-1773857006.jpg" alt="94% doanh nhân tại Việt Nam dự báo kinh tế sẽ khởi sắc trong năm nay">
+                                    </a>
+                                </div>
+                                <div class="content">
+                                    <p class="meta-news">
+                                        <a class="author-meta" href="https://vntravel.org.vn/user/tuyetvan">
+                                            <img class="user-avatar" src="https://vntravel.org.vn/zoom/156x0/uploads/images/Banbientap/2025/04/20/d912014a-003c-454c-bb30-9d4ecccc8316-1745123818.jpg" alt="user-avatar">Tuyết Vân </a>
+                                        <span class="time-public">1 ngày trước</span>
+                                    </p>
+                                    <h3 class="title fs5 f-rsb">
+                                        <a href="https://vntravel.org.vn/94-doanh-nhan-tai-viet-nam-du-bao-kinh-te-se-khoi-sac-trong-nam-nay-a8342.html" title="94% doanh nhân tại Việt Nam dự báo kinh tế sẽ khởi sắc trong năm nay"> 94% doanh nhân tại Việt Nam dự báo kinh tế sẽ khởi sắc trong năm nay </a>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <!--article_style_image.blade.php-->
+                            <div class="box-news grid-item match-height  kind-van-ban" style="height: 259.375px;">
+                                <div class="image image-wrapper">
+                                    <a href="https://vntravel.org.vn/quan-ly-housekeeping-trong-thoi-dai-so-nang-chuan-van-hanh-khach-san-viet-a8339.html" class="image image-small" title="Quản lý Housekeeping trong thời đại số: Nâng chuẩn vận hành khách sạn Việt">
+                                        <img src="https://vntravel.org.vn/zoom/480x288/uploads/images/blog/haingan116/2026/03/18/1-1758781169-1773802971.jpg" alt="Quản lý Housekeeping trong thời đại số: Nâng chuẩn vận hành khách sạn Việt">
+                                    </a>
+                                </div>
+                                <div class="content">
+                                    <p class="meta-news">
+                                        <a class="author-meta" href="https://vntravel.org.vn/user/haingan116">
+                                            <img class="user-avatar" src="https://vntravel.org.vn/zoom/156x0/uploads/images/haingan116/2025/07/25/485168043-4060054997601516-2652204820897826730-n-1753417635.jpg" alt="user-avatar">Trần Như Hải Ngân </a>
+                                        <span class="time-public">1 ngày trước</span>
+                                    </p>
+                                    <h3 class="title fs5 f-rsb">
+                                        <a href="https://vntravel.org.vn/quan-ly-housekeeping-trong-thoi-dai-so-nang-chuan-van-hanh-khach-san-viet-a8339.html" title="Quản lý Housekeeping trong thời đại số: Nâng chuẩn vận hành khách sạn Việt"> Quản lý Housekeeping trong thời đại số: Nâng chuẩn vận hành khách sạn Việt </a>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <!--article_style_image.blade.php-->
+                            <div class="box-news grid-item match-height  kind-normal" style="height: 259.375px;">
+                                <div class="image image-wrapper">
+                                    <a href="https://vntravel.org.vn/so-hoa-di-san-van-hoa-cau-noi-luu-giu-gia-tri-van-hoa-voi-hien-dai-a8334.html" class="image image-small" title="Số hóa di sản văn hóa - 'cầu nối' lưu giữ giá trị văn hóa với hiện đại">
+                                        <img src="https://vntravel.org.vn/zoom/480x288/uploads/blog_guest/images/2026/03/17/ha-noi-172836791107219442074-17736522129931523521725-1773730653.jpg" alt="Số hóa di sản văn hóa - 'cầu nối' lưu giữ giá trị văn hóa với hiện đại">
+                                    </a>
+                                </div>
+                                <div class="content">
+                                    <p class="meta-news">
+                                        <a class="author-meta" href="https://vntravel.org.vn/user/hoangdung">
+                                            <img class="user-avatar" src="https://vntravel.org.vn/zoom/156x0/uploads/profile-photos/ALqa23OXopEP2W6vg4kVSEeyFPYIrjGSetlI43t2.jpg" alt="user-avatar">Hoàng Dũng </a>
+                                        <span class="time-public">2 ngày trước</span>
+                                    </p>
+                                    <h3 class="title fs5 f-rsb">
+                                        <a href="https://vntravel.org.vn/so-hoa-di-san-van-hoa-cau-noi-luu-giu-gia-tri-van-hoa-voi-hien-dai-a8334.html" title="Số hóa di sản văn hóa - 'cầu nối' lưu giữ giá trị văn hóa với hiện đại"> Số hóa di sản văn hóa - 'cầu nối' lưu giữ giá trị văn hóa với hiện đại </a>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
     @include('partials.blocks.tabs-highlight')
 
     {{ sage_prefetch_link_posts($wp_query->posts ?? []) }}
